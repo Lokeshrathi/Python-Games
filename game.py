8b55d1a471df6f8b685c0e15a1aebe2ab34f033d
Lokesh Rathi <38494676+Lokeshrathi@users.noreply.github.com>
2020-10-20 22:52:59 +0530

    Game Code

---
+++ game.py
@@ -0,0 +1,42 @@
+import random
+def hangman(word): #create a function
+    wrong = 0
+    stages = ["",
+             "________________",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n"
+              .join(stages[0: wrong]))
+        print("You lose! It was {}.".format(word))
+
+words = ['Dance','Sing','Play','Sleep']
+result = random.choice(words)
+hangman(result)
